9bd520203f574ef45fa7c539974d6f3a172b8283
Elias Rodrigues <49626719+eliasantoniorodrigues1@users.noreply.github.com>
2022-03-22 08:09:14 -0300

    Add files via upload

---
+++ PROCURA TABELA PELO NOME DA COLUNA.sql
@@ -0,0 +1,16 @@
+SELECT      c.name  AS 'ColumnName'
+            ,t.name AS 'TableName'
+FROM        sys.columns c
+JOIN        sys.tables  t   ON c.object_id = t.object_id
+WHERE       c.name LIKE '%MUNCOD%'
+ORDER BY    TableName
+            ,ColumnName
+
+/*
+SELECT      COLUMN_NAME AS 'ColumnName'
+            ,TABLE_NAME AS  'TableName'
+FROM        INFORMATION_SCHEMA.COLUMNS
+WHERE       COLUMN_NAME LIKE '%MyName%'
+ORDER BY    TableName
+            ,ColumnName;
+			*/
